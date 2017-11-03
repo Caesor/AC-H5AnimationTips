b82eb95b7b1167bbddc49ef10600e9556134ec7e
nemoliao(廖泽恺) <nemoliao@tencent.com>
2017-11-03 10:30:36 +0800

    update

--- performance/frameAnimation.js
+++ performance/frameAnimation.js
@@ -1,26 +1,33 @@
+/* eslint no-magic-numbers:0 */
 /* polyfill */
-(function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+function RAF() {
+    let lastTime = 0;
+    const vendors = ['ms', 'moz', 'webkit', 'o'];
+    for (let x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
     }
 
-    if (!window.requestAnimationFrame)
+    if (!window.requestAnimationFrame) {
         window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
+            let currTime = new Date().getTime();
+            let timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            let id = window.setTimeout(function() { callback(currTime + timeToCall) }, timeToCall);
             lastTime = currTime + timeToCall;
             return id;
         };
+    }
 
-    if (!window.cancelAnimationFrame)
+    if (!window.cancelAnimationFrame) {
         window.cancelAnimationFrame = function(id) {
             clearTimeout(id);
         };
-}());
+    }
+}
+RAF();
+
+const STANDARDFPS = 60;
+const SECOND = 1000;
 
 class FrameAnimate {
 
@@ -32,12 +39,14 @@ class FrameAnimate {
         this.width = opts.width || window.screen.availWidth;
         this.height = opts.height || window.screen.availHeight;
         this.frame = opts.frame;
-        this.fps = opts.fps || 60;
+        this.fps = opts.fps || STANDARDFPS;
         this.loop = opts.loop || 1;
         this.pixelRatio = opts.pixelRatio || 1;
         this.source = opts.source;
         this.singleMode = opts.singleMode || false;
+        this.config = opts.config;
         this.callback = callback;
+        this.parallel = opts.parallel;
 
         this.timer;
         this.start = null;
@@ -50,37 +59,47 @@ class FrameAnimate {
     }
 
     init() {
-        this.canvas.width = this.screenWith;
-        this.canvas.height = this.screenHeight;
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
         this.ctx = this.canvas.getContext('2d');
         this.loadResource(this.singleMode);
     }
 
     loadResource(single) {
-        if(single) {
+        if (single) {
             console.log('single mode');
             this._img = new Image();
             this._img.src = this.source[0];
             this._img.onload = () => {
-                const img_WIDTH = this._img.width / this.width;
-                const img_HEIGHT = this._img.height / this.height;
-                for (let i = 0; i < img_HEIGHT; i++) {
-                    for (let j = 0; j < img_WIDTH; j++) {
-                        this.frameArray.push({
-                            x: (j % img_WIDTH) * this.width,
-                            y: i * this.height
-                        });
+                // normal single model and config single model
+                if (this.config) {
+                    for (let i = 0; i < this.config.length; i++) {
+                        this.frameArray.push(this.config[i]);
                     }
+                    this.onloadLength++;
+                    this.frame = this.config.length;
+                    // debugger
+                } else {
+                    const img_WIDTH = this._img.width / this.width;
+                    const img_HEIGHT = this._img.height / this.height;
+                    for (let i = 0; i < img_HEIGHT; i++) {
+                        for (let j = 0; j < img_WIDTH; j++) {
+                            this.frameArray.push({
+                                x: (j % img_WIDTH) * this.width,
+                                y: i * this.height
+                            });
+                        }
+                    }
+                    this.onloadLength++;
+                    this.frame = img_HEIGHT * img_WIDTH;
                 }
-                this.onloadLength++;
-                this.frame = img_HEIGHT * img_WIDTH;
             };
-        }else {
+        } else {
             console.log('frame mode');
             for (let i = 0; i < this.frame; i++) {
                 let _img = new Image();
                 _img.src = this.source[i];
-    
+
                 _img.onload = () => {
                     this.onloadLength++;
                     this.frameArray.push(_img);
@@ -89,46 +108,58 @@ class FrameAnimate {
         }
     }
 
-    play() {
+    play(n = 0) {
         this.pause();
+        n && (this.currentFrame = n);
         if (this.onloadLength < this.picNum) {
-            this.checkLoad('play');
+            this.checkLoad(n);
         } else {
+            this.parallel && this.parallel();
+            this.canvas.style.backgroundImage = 'none';
             window.requestAnimationFrame(this.draw.bind(this));
         }
     }
 
-    checkLoad(next, n) {
-        this.timer = setInterval(() => {
-            if (this.onloadLength === this.picNum) {
-            console.log("pics load complete");
-                clearInterval(this.timer);
-                switch (next) {
-                    case 'draw':
-                        this.draw();
-                        break;
-                    case 'seekTo':
-                        this.seekTo(n);
-                        break;
-                    default:
-                        this.play();
-                }
-            }
-        }, 1000 / this.fps);
+    drawOneFrame(nth) {
+        this.pause();
+        this.currentFrame = nth;
+        if (this.onloadLength < this.frame) {
+            this.checkLoad(nth, false);
+        } else {
+            this.canvas.style.backgroundImage = 'none';
+            this.oneFrame = true;
+            this.draw(nth);
+        }
     }
 
-    draw(timestamp) {
+    checkLoad(n = 0, autoPlay = true) {
+        this.timer = setInterval(() => {
+            if (this.onloadLength === this.picNum) {
+                clearInterval(this.timer);
+                if (autoPlay) {
+                    this.play(n);
+                } else {
+                    this.drawOneFrame(n);
+                }
+            }
+        }, SECOND / this.fps);
+    }
+
+    draw(timestamp, func) {
         !this.start && (this.start = timestamp);
 
         // 如果没有限定帧率，使用 RAF 自有发挥
-        let raf = this.fps === 60 ? true : (timestamp - this.start > 1000 / this.fps);
-        
-        if (raf) {
+        let raf = this.fps === STANDARDFPS ? true : (timestamp - this.start > SECOND / this.fps);
+        if (raf || this.oneFrame) {
             this.ctx.clearRect(0, 0, this.width, this.height);
             const f = this.frameArray[this.currentFrame];
-            const {x, y} = f;
+            const { x, y, w = this.width, h = this.height, offX = 0, offY = 0, sW = this.width, sH = this.height } = f;
             if (this.singleMode) {
-                this.ctx.drawImage(this._img, x, y, this.width, this.height, (this.screenWith - this.width/2)/2, (this.screenHeight - this.height/2)/2, this.width/2, this.height/2);
+                const dx = offX * (this.width / sW);
+                const dy = offY * (this.height / sH);
+                const dw = w * (this.width / sW);
+                const dh = h * (this.height / sH);
+                this.ctx.drawImage(this._img, x, y, w, h, dx, dy, dw, dh);
             } else {
                 let pic_width = parseInt(f.width / 2, 10);
                 let pic_height = parseInt(f.height / 2, 10);
@@ -138,12 +169,28 @@ class FrameAnimate {
             this.currentFrame++;
         }
 
+        // 只绘制指定一帧
+        if (this.oneFrame) {
+            return;
+        }
+
+        func && func();
+
         if (this.currentFrame === this.frame - 1) {
             this.currentFrame = 0;
-            this.loop--;
-            if (this.loop === 0) {
+            if (typeof this.loop === 'number') {
+                this.loop--;
+                if (this.loop === 0) {
+                    this.pause();
+                    this.callback && this.callback();
+                    return;
+                }
+            } else if (this.loop === 'infinity') {
+                this.rafTimer = window.requestAnimationFrame(this.draw.bind(this));
+            } else {
                 this.pause();
                 this.callback && this.callback();
+                return;
             }
         } else {
             this.rafTimer = window.requestAnimationFrame(this.draw.bind(this));
@@ -152,28 +199,16 @@ class FrameAnimate {
 
     replay() {
         this.pause();
-        this.seekTo(0);
-        this.play();
+        this.play(0);
     }
 
     stop() {
         this.pause();
-        this.seekTo(this.frame - 1);
+        this.play(this.frame - 1);
     }
 
     pause() {
         this.timer && clearInterval(this.timer);
         this.rafTimer && window.cancelAnimationFrame(this.rafTimer);
     }
-
-    seekTo(n) {
-        this.pause();
-        this.currentFrame = n;
-        if (this.onloadLength < this.frame) {
-            this.checkLoad('seekTo', n);
-        } else {
-            this.draw();
-        }
-    }
-
 }
